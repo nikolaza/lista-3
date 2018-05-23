99dfdcb36d47936b1dcf8d187bf422ca87a6306c
Nikola Zafirovski <nikola.zafirovski@gmail.com>
2018-05-23 20:51:02 +0200

    Prv komit!

---
+++ src/Pocetna.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pocetna {
+
+	public static void main(String[] args) {
+		List<String> iminja;
+		iminja = new ArrayList<>();
+		iminja.add("nikola");
+		iminja.add("PPPP");
+		iminja.add("mmmmm");
+		iminja.add("MMM");
+		
+		System.out.println(iminja);
+		System.out.println(iminja.size());
+		
+	}	
+
+}
